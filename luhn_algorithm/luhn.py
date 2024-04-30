357b9744793526439b584b733a932a9dcbe56bf0
oktavianuu <oktavianuss.misro@gmail.com>
2024-04-30 08:51:48 +0700

    luhn algorithm.txt

--- luhn_algorithm/luhn.py
+++ luhn_algorithm/luhn.py
@@ -1,5 +1,12 @@
+def verify_card_number(card_number):
+    pass
+
 def main():
     card_number = '4111-1111-4555-1142'
     card_translation = str.maketrans({'-':'', ' ':'' })
     translated_card_number = card_number.translate(card_translation)
+    verify_card_number(translated_card_number)
+    print(translated_card_number)
+
+main()
     
