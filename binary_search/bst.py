8e3ed0010fdb24011300bf470fd2d14c7db46813
oktavianuu <oktavianuss.misro@gmail.com>
2024-07-08 18:38:35 +0700

    class and objects

--- binary_search/bst.py
+++ binary_search/bst.py
@@ -87,3 +87,5 @@ print("Inorder traversal:", bst.inorder_traversal())
 bst.delete(40)
 
 print("Search for 40:", bst.search(40))
+
+print('Inorder traversal after deleting 40:', bst.inorder_traversal())
