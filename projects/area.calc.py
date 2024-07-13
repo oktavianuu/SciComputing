6e3263cc13dbc5bd083f39ef3fa68c2f925f19cc
oktavianuu <oktavianuss.misro@gmail.com>
2024-07-13 18:10:59 +0700

    updatte area calc project

--- projects/area.calc.py
+++ projects/area.calc.py
@@ -1,88 +1,47 @@
-class R2Vector:
-    def __init__(self, *, x, y):
-        self.x = x
-        self.y = y
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
-    def norm(self):
-        return sum(val**2 for val in vars(self).values())**0.5
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        return ("*" * self.width + "\n") * self.height
+
+    def get_amount_inside(self, shape):
+        return (self.width // shape.width) * (self.height // shape.height)
 
     def __str__(self):
-        return str(tuple(getattr(self, i) for i in vars(self)))
+        return f"Rectangle(width={self.width}, height={self.height})"
 
-    def __repr__(self):
-        arg_list = [f'{key}={val}' for key, val in vars(self).items()]
-        args = ', '.join(arg_list)
-        return f'{self.__class__.__name__}({args})'
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
 
-    def __add__(self, other):
-        if type(self) != type(other):
-            return NotImplemented
-        kwargs = {i: getattr(self, i) + getattr(other, i) for i in vars(self)}
-        return self.__class__(**kwargs)
+    def set_side(self, side):
+        self.width = side
+        self.height = side
 
-    def __sub__(self, other):
-        if type(self) != type(other):
-            return NotImplemented
-        kwargs = {i: getattr(self, i) - getattr(other, i) for i in vars(self)}
-        return self.__class__(**kwargs)
+    def set_width(self, width):
+        self.set_side(width)
 
-    def __mul__(self, other):
-        if type(other) in (int, float):
-            kwargs = {i: getattr(self, i) * other for i in vars(self)}
-            return self.__class__(**kwargs)        
-        elif type(self) == type(other):
-            args = [getattr(self, i) * getattr(other, i) for i in vars(self)]
-            return sum(args)            
-        return NotImplemented
+    def set_height(self, height):
+        self.set_side(height)
 
-    def __eq__(self, other):
-        if type(self) != type(other):
-            return NotImplemented
-        return all(getattr(self, i) == getattr(other, i) for i in vars(self))
-        
-    def __ne__(self, other):
-        return not self == other
-
-    def __lt__(self, other):
-        if type(self) != type(other):
-            return NotImplemented
-        return self.norm() < other.norm()
-
-    def __gt__(self, other):
-        if type(self) != type(other):
-            return NotImplemented
-        return self.norm() > other.norm()
-
-    def __le__(self, other):
-        return not self > other
-
-    def __ge__(self, other):
-        return not self < other
-
-class R3Vector(R2Vector):
-    def __init__(self, *, x, y, z):
-        super().__init__(x=x, y=y)
-        self.z = z
-        
-    def cross(self, other):
-        if type(self) != type(other):
-            return NotImplemented
-        kwargs = {
-            'x': self.y * other.z - self.z * other.y,
-            'y': self.z * other.x - self.x * other.z,
-            'z': self.x * other.y - self.y * other.x
-        }
-        
-        return self.__class__(**kwargs)
-v1 = R3Vector(x=2, y=3, z=1)
-v2 = R3Vector(x=0.5, y=1.25, z=2)
-print(f'v1 = {v1}')
-print(f'v2 = {v2}')
-v3 = v1 + v2
-print(f'v1 + v2 = {v3}')
-v4 = v1 - v2
-print(f'v1 - v2 = {v4}')
-v5 = v1 * v2
-print(f'v1 * v2 = {v5}')
-v6 = v1.cross(v2)
-print(f'v1 x v2 = {v6}')
+    def __str__(self):
+        return f"Square(side={self.width})"
